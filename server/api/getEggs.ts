f162851433d190866251ceaa5543f8fad5cb493f
Jeremiah Gage <jgage6@oreillyauto.com>
2024-03-27 16:07:01 -0500

    Fifth commit

--- server/api/getEggs.ts
+++ server/api/getEggs.ts
@@ -6,37 +6,41 @@ export default defineEventHandler(
   requireAuth(async (event) => {
     const username = await getUser(event);
     const egg = await readBody(event);
-    if (!!egg && !!egg.coords && !!egg.coords.lat && !!egg.coords.lng) {
-      return await prisma.egg.findMany({
-        where: {
-          coords: {
-            lat: { lte: egg.coords.lat + 0.1, gte: egg.coords.lat - 0.1 },
-            lng: { lte: egg.coords.lng + 0.1, gte: egg.coords.lng - 0.1 },
+    try {
+      if (!!egg && !!egg.coords && !!egg.coords.lat && !!egg.coords.lng) {
+        return await prisma.egg.findMany({
+          where: {
+            coords: {
+              lat: { lte: egg.coords.lat + 0.1, gte: egg.coords.lat - 0.1 },
+              lng: { lte: egg.coords.lng + 0.1, gte: egg.coords.lng - 0.1 },
+            },
+            isCollected: false,
           },
-          isCollected: false,
-        },
-        include: {
-          coords: {},
-        },
-      });
-    } else if (!!egg && !!egg.id) {
-      return await prisma.egg.findUnique({
-        where: {
-          id: egg.id,
-        },
-        include: {
-          coords: {},
-        },
-      });
-    } else {
-      return await prisma.egg.findMany({
-        where: {
-          username: username,
-        },
-        include: {
-          coords: {},
-        },
-      });
+          include: {
+            coords: {},
+          },
+        });
+      } else if (!!egg && !!egg.id) {
+        return await prisma.egg.findUnique({
+          where: {
+            id: egg.id,
+          },
+          include: {
+            coords: {},
+          },
+        });
+      } else {
+        return await prisma.egg.findMany({
+          where: {
+            username: username,
+          },
+          include: {
+            coords: {},
+          },
+        });
+      }
+    } catch (error: unknown) {
+      console.error(error);
     }
   }),
 );
