8fcaea24ad0a664be406e2b66dc0d3ba2da3ade4
Jeremiah Gage <jgage6@oreillyauto.com>
2024-03-29 15:42:26 -0500

    Print styling

--- pages/Codes.vue
+++ pages/Codes.vue
@@ -2,16 +2,28 @@
 import { useGetUserEggs } from '~/composables/gateway/egg';
 import { parseLinkFromEgg } from '~/composables/parseLinkFromEgg';
 import QrcodeVue from 'qrcode.vue';
+import { randomTailwindBackgroundColor } from '~/composables/colors/randomPastelColors';
 import type { Level, RenderAs } from 'qrcode.vue';
 
 const [eggs, error] = useGetUserEggs();
 const level = ref<Level>('M');
 const renderAs = ref<RenderAs>('svg');
+const color = randomTailwindBackgroundColor();
+const handlePrint = () => {
+  window.print();
+};
 </script>
 
 <template>
-  <div class="h-fit flex flex-wrap gap-4 my-24 items-center justify-center">
-    <div v-for="egg in eggs" :key="egg.id" class="w-64 flex flex-col items-center justify-center gap-4">
+  <button
+    @click="handlePrint"
+    id="printer"
+    :class="`w-16 h-16 p-2 border border-black shadow-xl aspect-square rounded-full fixed top-24 right-6 active:opacity-80 ${color}`"
+  >
+    <vPrinter />
+  </button>
+  <section id="item-pane" class="h-fit flex flex-wrap gap-4 my-24 items-center justify-center">
+    <div v-for="egg in eggs" :key="egg.id" id="item" class="w-64 flex flex-col items-center justify-center gap-4">
       <vSubtitle class="w-32">{{ egg.title }}</vSubtitle>
       <span
         class="m-4 mt-0 p-4 border-black border-2 shadow-xl flex flex-col items-center justify-center"
@@ -22,5 +34,27 @@ const renderAs = ref<RenderAs>('svg');
         </span>
       </span>
     </div>
-  </div>
+  </section>
 </template>
+
+<style>
+@media print {
+  #printer {
+    display: none;
+  }
+  #item-pane {
+    padding: 0;
+    gap: 0;
+    margin: 0;
+    break-inside: avoid-page;
+    height: fit-content;
+    padding-bottom: 100px;
+  }
+  #item {
+    break-inside: avoid-page !important;
+    break-before: always;
+    print-color-adjust: exact;
+    box-shadow: none;
+  }
+}
+</style>
